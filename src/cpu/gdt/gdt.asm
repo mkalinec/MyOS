d4d5ca9fd0efb8a2415a2352799b2a0f9b52e41a
Martin <kalinec.martin@proton.me>
2026-01-12 18:01:59 +0100

    finaly fixet gdt - at least it seem like

--- src/cpu/gdt/gdt.asm
+++ src/cpu/gdt/gdt.asm
@@ -1,21 +1,17 @@
 global gdt_flush
 
 gdt_flush:
-    ; rdi = pointer to gdt_ptr
-    lgdt [rdi]               ; Load GDTR
+    lgdt [rdi]
 
-    mov ax, 0x10             ; Kernel data segment
+    mov ax, 0x10 ; Kernel data segment
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
+    pop rdi
 
-    ; Far jump to reload CS
-    push 0x08                    ; Kernel code selector
-    lea rax, [rel flush_target]  ; Load address of label
+    mov rax, 0x08 ; Kernel code segment
     push rax
-    lretq                        ; 64-bit far return
-
-flush_target:
-    ret
+    push rdi
+    retfq
